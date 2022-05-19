54128589cf549473728f5196b3e826614534c727
Matas Rastenis <matas2k@gmail.com>
2022-05-19 15:39:15 +0300

    Add more data to summary

--- analysis/extractSummary.py
+++ analysis/extractSummary.py
@@ -5,9 +5,11 @@ filename=sys.argv[1]
 
 print("Showing summary for file file {} ...".format(filename))
 
-
+total_count=0
 pr_count=0
 bug_count=0
+pr_label_count=0
+verified_count=0
 
 with open(filename) as f:
     data = json.load(f)
@@ -15,8 +17,16 @@ with open(filename) as f:
     for ob in data:
         labelsMapped=list(map(lambda o: o["name"],ob["data"]["labels"]))
 
+        total_count=total_count+1
+
         if "bug" not in labelsMapped:
             continue
+
+        if "has_pr" in labelsMapped:
+            pr_label_count=pr_label_count+1
+
+        if "verified" in labelsMapped:
+            verified_count=verified_count+1
 
         bug_count=bug_count+1
         print("=====")
@@ -28,5 +38,9 @@ with open(filename) as f:
             print("<No PR>")
         print("=====")
 
+print("{} total issues.".format(total_count))
 print("{} Bugs.".format(pr_count))
 print("{} Bugs with PRs.".format(pr_count))
+print("{} Verified bugs.".format(verified_count))
+print("{} Bugs with the 'has_pr' label.".format(pr_label_count))
+
